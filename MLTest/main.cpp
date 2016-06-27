f559321bd6e617391e184f94524e3c5d2ba4e78f
Sofiya Klyan <sofi.kljan@gmail.com>
2016-06-27 18:08:54 +0300

    added command line args support

--- MLTest/main.cpp
+++ MLTest/main.cpp
@@ -1,4 +1,11 @@
 #include "mnist_test.h"
+#include <string>
+
+using namespace std;
+
+/* usage ex: <app_name> --train-inputs D:\IT\Data\train-images.idx3-ubyte --train-outputs D:\IT\Data\train-labels.idx1-ubyte  
+--test-inputs D:\IT\Data\t10k-images.idx3-ubyte --test-outputs D:\IT\Data\t10k-labels.idx1-ubyte 
+*/
 
 int main(int argc, char* argv[])
 {
@@ -7,8 +14,28 @@ int main(int argc, char* argv[])
 	char* labelsFile = "D:\\IT\\Data\\train-labels.idx1-ubyte";
 	char* testLabelsFile = "D:\\IT\\Data\\t10k-labels.idx1-ubyte";
 	
-	const int max_img_number = 2000;
+	int max_img_number = 2000;
 
+	for (int i_argNum = 1; i_argNum < argc; ++i_argNum)
+	{
+		if (string(argv[i_argNum]) == "--train-inputs")
+		{
+			imagesFile = argv[++i_argNum];
+		}
+		else if (string(argv[i_argNum]) == "--train-outputs")
+		{
+			labelsFile = argv[++i_argNum];
+		}
+		if (string(argv[i_argNum]) == "--test-inputs")
+		{
+			testImagesFile = argv[++i_argNum];
+		}
+		else if (string(argv[i_argNum]) == "--test-outputs")
+		{
+			testLabelsFile = argv[++i_argNum];
+		}
+	}
+	
 	MNISTTester* test = new MNISTTester(imagesFile, labelsFile, testImagesFile, testLabelsFile, max_img_number, max_img_number);
 	test->Process();
 	
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # dummy tests
 
+Usage:
+mltest --train-inputs train-images.idx3-ubyte --train-outputs train-labels.idx1-ubyte --test-inputs t10k-images.idx3-ubyte --test-outputs t10k-labels.idx1-ubyte 
+
 TODO:
 1) change output type to int
-2) implement Save and Load for KNN (keeping k, classNum, featureNum, train inputs with corresponding outputs)
-4) add "honest" implementation of PCA
-5) add command lne arguments usage in main
-6) check if pca back propagation returns good images
+2) Add support for any classes
+3) add "honest" implementation of PCA
+4) check if pca back propagation returns good images
